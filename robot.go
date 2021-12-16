4e79b626be07f0e8c5e1073242796d9e4abbceb2
zengchen1024 <chenzeng2@huawei.com>
2021-12-16 14:45:25 +0800

    update with new lib

--- robot.go
+++ robot.go
@@ -3,10 +3,9 @@ package main
 import (
 	"errors"
 
-	sdk "gitee.com/openeuler/go-gitee/gitee"
-	libconfig "github.com/opensourceways/community-robot-lib/config"
-	"github.com/opensourceways/community-robot-lib/giteeclient"
-	libplugin "github.com/opensourceways/community-robot-lib/giteeplugin"
+	"github.com/opensourceways/community-robot-lib/config"
+	"github.com/opensourceways/community-robot-lib/robot-gitee-framework"
+	sdk "github.com/opensourceways/go-gitee/gitee"
 	"github.com/sirupsen/logrus"
 )
 
@@ -26,34 +25,31 @@ type robot struct {
 	botName string
 }
 
-func (bot *robot) NewPluginConfig() libconfig.PluginConfig {
+func (bot *robot) NewConfig() config.Config {
 	return &configuration{}
 }
 
-func (bot *robot) getConfig(cfg libconfig.PluginConfig) (*configuration, error) {
+func (bot *robot) getConfig(cfg config.Config) (*configuration, error) {
 	if c, ok := cfg.(*configuration); ok {
 		return c, nil
 	}
 	return nil, errors.New("can't convert to configuration")
 }
 
-func (bot *robot) RegisterEventHandler(p libplugin.HandlerRegitster) {
-	p.RegisterNoteEventHandler(bot.handleNoteEvent)
+func (bot *robot) RegisterEventHandler(f framework.HandlerRegitster) {
+	f.RegisterNoteEventHandler(bot.handleNoteEvent)
 }
 
-func (bot *robot) handleNoteEvent(e *sdk.NoteEvent, pc libconfig.PluginConfig, log *logrus.Entry) error {
-
-	config, err := bot.getConfig(pc)
+func (bot *robot) handleNoteEvent(e *sdk.NoteEvent, c config.Config, log *logrus.Entry) error {
+	config, err := bot.getConfig(c)
 	if err != nil {
 		return err
 	}
 
-	ne := giteeclient.NewIssueNoteEvent(e)
-
-	cfg := config.configFor(ne.GetOrgRep())
+	cfg := config.configFor(e.GetOrgRepo())
 	if cfg == nil {
 		return nil
 	}
 
-	return bot.handleSweepstakes(ne, cfg)
+	return bot.handleSweepstakes(e, cfg)
 }
